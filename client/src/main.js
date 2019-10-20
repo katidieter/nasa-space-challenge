66a31ed9c83d73cf008d3787afad073723666515
Katieli Bianca Dieter <katielidieter@hotmail.com>
2019-10-20 13:59:24 -0300

    Add google maps to project

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import * as VueGoogleMaps from 'vue2-google-maps';
 import App from './App.vue';
 import routes from './routes';
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBgXgt7_49qViyPRmopYsToHHRyvDJOrqc',
+    libraries: 'places,drawing,visualization',
+  },
+});
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
