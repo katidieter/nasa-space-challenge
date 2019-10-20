a2c350a1f86c977c1a328fc9b90ef496869829f0
Katieli Bianca Dieter <katielidieter@hotmail.com>
2019-10-20 13:42:21 -0300

    Add method to fetch disasters

--- client/src/components/DisasterMap.vue
+++ client/src/components/DisasterMap.vue
@@ -1,20 +1,40 @@
 <template>
   <div id="disaster-map">
     <h1>AQUI VAI VIM O MAPA</h1>
+    <h2>{{disasters[0].categories[0].title}}</h2>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 
+const CATEGORIES = [
+  { id: 9, name: 'Floods' },
+  { id: 10, name: 'Severe Storms' },
+  { id: 14, name: 'Landslides' },
+];
+
 export default {
   name: 'DisasterMap',
+  data() {
+    return {
+      disasters: [],
+      loading: true,
+    };
+  },
   created() {
-    axios
-      .get('https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/8?status=open')
-      .then((response) => {
-        console.log(response.data.events);
-      });
+    Promise.all(
+      CATEGORIES.map(category => this.fetchDisasterByCategoryId(category.id)),
+    ).then(() => console.log(this.disasters));
+  },
+  methods: {
+    fetchDisasterByCategoryId(id) {
+      return axios
+        .get(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/${id}?status=closed`)
+        .then((response) => {
+          this.disasters.push(...response.data.events);
+        });
+    },
   },
 };
 </script>
